c706f57c434a6ccc36fb032b443cfdb70e3f7f81
petro-mykhats2 <02webproject02@gmail.com>
2022-09-26 22:21:37 +0200

    modified netlify function

---
+++ src/pages/blog/2022-09-21-my-news copy.md
@@ -0,0 +1,13 @@
+---
+templateKey: blog-post
+title: My news test from vcode
+date: 2022-09-21T21:05:49.913Z
+description: jhjhjjmnbmnbmbm nbmnbmnbmbmn bmnbmnbmbmnbmnbmn bmbmnbmn bmnbmbmnbmnbmnbmbmnb
+
+featuredpost: true
+featuredimage: /img/image.jpeg
+tags:
+  - ytytyty
+---
+
+r﻿rrrrrrr
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,115 +1,83 @@
-import * as React from "react";
-import { navigate } from "gatsby-link";
-import Layout from "../../components/Layout";
+import React from "react"
+// import { connect, useSelector } from "react-redux";
+// import { clearCart } from "../redux/cartreducer";
+import { useForm } from "react-hook-form"
+import Layout from "../../components/Layout"
+// import { Logger } from "sass"
+// import moment from "moment";
+import fetch from "cross-fetch"
 
-function encode(data) {
-  return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&");
-}
+const CartForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+  // const [toServer, SettoServer] = useState("");
+  // const dateNow = moment(Date.now()).format("DD-MM-YYYY");
+  // const books = useSelector(state => state.cartreducer);
 
-export default class Index extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isValidated: false };
-  }
-
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const form = e.target;
-    fetch("/", {
+  const onSubmit = (data) => {
+    console.log("data::", data)
+    const requestOptions = {
       method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": form.getAttribute("name"),
-        ...this.state,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: data.name,
+        phone: data.phone,
+        email: data.email,
       }),
-    })
-      .then(() => navigate(form.getAttribute("action")))
-      .catch((error) => alert(error));
-  };
-
-  render() {
-    return (
-      <Layout>
-        <section className="section">
-          <div className="container">
-            <div className="content">
-              <h1>Contact</h1>
-              <form
-                name="contact"
-                method="post"
-                action="/contact/thanks/"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
-                onSubmit={this.handleSubmit}
-              >
-                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-                <input type="hidden" name="form-name" value="contact" />
-                <div hidden>
-                  <label>
-                    Don’t fill this out:{" "}
-                    <input name="bot-field" onChange={this.handleChange} />
-                  </label>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={"name"}>
-                    Your name
-                  </label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type={"text"}
-                      name={"name"}
-                      onChange={this.handleChange}
-                      id={"name"}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={"email"}>
-                    Email
-                  </label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type={"email"}
-                      name={"email"}
-                      onChange={this.handleChange}
-                      id={"email"}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={"message"}>
-                    Message
-                  </label>
-                  <div className="control">
-                    <textarea
-                      className="textarea"
-                      name={"message"}
-                      onChange={this.handleChange}
-                      id={"message"}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <button className="button is-link" type="submit">
-                    Send
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </section>
-      </Layout>
-    );
+    }
+    fetch("/.netlify/functions/hello", requestOptions)
+      .then((response) => response.json())
+      .then(console.log)
   }
+
+  return (
+    <Layout>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="Як вас звати?"
+          maxLength="21"
+          id="name"
+          {...register("name", {
+            required: "Введіть сюди Ваше імя",
+            minLength: 2,
+          })}
+        />
+        {errors.name && <div className="error">Введіть Ваше імя</div>}
+        <input
+          // className="input-cart"
+          type="tel"
+          placeholder="Введіть номер телефону"
+          id="phone"
+          // name="phone"
+          {...register("phone", {
+            required: "Введіть номер телефону",
+            minLength: 9,
+          })}
+        />
+        {errors.phone && (
+          <div className="error">Введіть коректний номер телефону</div>
+        )}
+        <input
+          type="text"
+          placeholder="Введіть пошту"
+          id="email"
+          {...register("email", {
+            required: "Введіть Email",
+            validate: (value) =>
+              value.includes("@") || "Введіть коректний Email",
+          })}
+        />
+        {errors.email && <div className="error"> {errors.email.message}</div>}
+        <input type="submit" />
+      </form>
+    </Layout>
+  )
 }
+
+export default CartForm
